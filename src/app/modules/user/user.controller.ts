548e48b188113552387b2e93ab9cf43f3de54e93
Erazul Islam <erazul89@gmail.com>
2025-04-16 21:05:45 +0600

    send response

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
 
-const createAdmin = async (req:Request,res:Response) => {
-    const data = req.body
-    const result = await userService.createAdmin(data)
-
-    res.send(result)
+const createAdmin = async (req: Request, res: Response) => {
+    try {
+        const data = req.body
+        const result = await userService.createAdmin(data)
+        res.status(200).json({
+            success: true,
+            message: "Admin created successfully",
+            data: result
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false,
+            message : "Something went wrong",
+            error : err
+        })
+    }
 }
 
 export const userController = {
